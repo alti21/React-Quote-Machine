23a09c9f6c51a5b909121be1f0a29ce6fee097d4
Altemush Bhatti <altemush@gmail.com>
2020-06-15 12:09:31 -0700

    Finished App Component

--- quote-machine/src/App.js
+++ quote-machine/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import quotes from './QuoteDB';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: quotes[0].quote,
+      author: quotes[0].author
+    }
+    this.generateQuote = this.generateQuote.bind(this);
+    this.sortQuotes = this.sortQuotes.bind(this);
+  }
+
+  generateQuote(arr) {
+    let num = Math.floor(Math.random() * quotes.length);
+
+    let newQuote = quotes[num];
+
+    this.setState({
+      quote: newQuote.quote,
+      author: newQuote.author
+    })
+
+    this.sortQuotes(quotes);
+  }
+
+  sortQuotes(arr) {
+    return arr.sort(function () { return 0.5 - Math.random() });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">Quote of the Moment</h1>
+        <QuoteAndAuthor
+          generateQuote={this.generateQuote}
+          quote={this.state}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
